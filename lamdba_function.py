c9b7ddd7965a878d9ee2dd512daf255fe1894104
ranjithreddy-31 <grreddy2726@gmail.com>
2024-02-23 22:00:53 -0600

    Lambda function

---
+++ lamdba_function.py
@@ -0,0 +1,25 @@
+import io
+import csv
+import boto3
+import json
+
+s3Client = boto3.client('s3')
+
+def lambda_handler(event, contest):
+    '''
+    This lambda function is triggered whenever there is a file upload to s3. It fetches data from that file and prints it.
+    '''
+    # Get Bucket and File name
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+
+    # Get our object
+    response = s3Client.get_object(Bucket=bucket, Key=key)
+
+    #Process and print the data
+    data = response['Body'].read.decod('utf-8')
+    reader = csv.reader(io.StringIO(data))
+    next(reader)
+    for row in data:
+        print(row)
+
